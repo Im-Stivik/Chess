16a4f9899f8644d543696d844ee7d46ba8857823
Im-Stivik <mogil.sharon@gmail.com>
2022-10-14 15:14:12 +0300

    made import and export for the game settings

--- GameSettings.cs
+++ GameSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 
 namespace Chess
@@ -17,5 +18,65 @@ namespace Chess
         public static Color OptionsColor = Color.Cyan;
         public static Color SelectedColor = Color.DimGray;
         public static Color EatPiesOptionColor = Color.Gold;
+
+        public static string GetSettingsText()
+        {
+            string result = "";
+            result += "BoardSize: " + BoardSize + "\n";
+            result += "PieceSize: " + PieceSize + "\n";
+            result += "BoardMarginLeft: " + BoardMarginLeft + "\n";
+            result += "BoardMarginTop: " + BoardMarginTop + "\n";
+            result += "LightColor: " + LightColor + "\n";
+            result += "DarkColor: " + DarkColor + "\n";
+            result += "OptionsColor: " + OptionsColor + "\n";
+            result += "SelectedColor: " + SelectedColor + "\n";
+            result += "EatPiesOptionColor: " + EatPiesOptionColor + "\n";
+            return result;
+        }
+
+        public static string SetSetting(string name, string value)
+        {
+            try
+            {
+                switch (name)
+                {
+                    case "BoardSize":
+                        BoardSize = int.Parse(value);
+                        break;
+                    case "PieceSize":
+                        PieceSize = int.Parse(value);
+                        break;
+                    case "BoardMarginLeft":
+                        BoardMarginLeft = int.Parse(value);
+                        break;
+                    case "BoardMarginTop":
+                        BoardMarginTop = int.Parse(value);
+                        break;
+                    case "LightColor":
+                        LightColor = Color.FromName(value);
+                        break;
+                    case "DarkColor":
+                        DarkColor = Color.FromName(value);
+                        break;
+                    case "OptionsColor":
+                        OptionsColor = Color.FromName(value);
+                        break;
+                    case "SelectedColor":
+                        SelectedColor = Color.FromName(value);
+                        break;
+                    case "EatPiesOptionColor":
+                        EatPiesOptionColor = Color.FromName(value);
+                        break;
+                    default:
+                        return "Unknown setting";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return null;
+        }
     }
 }
